2d57aa51b1a0202959b64008f43209a3d87e1d49
xyz1817 <anayaramane@gmail.com>
2026-01-28 21:36:05 +0530

    Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -1,4 +1,18 @@
 #include<stdio.h>
 void main()
-{ printf("Hello world")
+{
+ int a;
+ printf("Enter a year=");
+ scanf("%d" ,&a);
+ if(a%100==0 && a%400==0);
+{
+  printf("It is a leap year");
 }
+  else if(a%4==0 && a%100!=0)
+  {
+    printf("It is a leap year");
+  else
+{
+  printf("It is not a leap year");
+}
+  }
